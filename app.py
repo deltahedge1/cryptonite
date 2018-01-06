499c00c2d02899178dfafc803576337b35a96b14
Hassan <ihassan1@kpmg.com.au>
2018-01-07 01:52:30 +1100

    adding db fields

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ class User(db.Model):
     email = db.Column(db.String(120), unique=True)
     password= db.Column(db.String(120))
 
-    def __init__(self, name, email):
+    def __init__(self, name, email,public_id, password):
         self.name = name
         self.email = email
         self.public_id = public_id
